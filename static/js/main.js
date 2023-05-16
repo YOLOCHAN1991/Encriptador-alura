c53cdf2422d7109045cb00dc50e8715890296205
gpe123 <riosmanuel1920@gmail.com>
2023-05-16 17:30:29 -0500

    final changes done.
    responsive web final changes completed.

--- static/js/main.js
+++ static/js/main.js
@@ -1,53 +1,58 @@
 
 
+var encryptDict =   {"e":"enter", 
+                    "i":"imes",
+                    "a":"ai",
+                    "o":"ober", 
+                    "u":"ufat"}
+
+
 function encriptador(texto) {
-    var textoEncriptado = "";
-    var letra;
-    for (var i = 0; i < texto.length; i++) {
-        letra = texto[i]
-        if (letra == "e") {
-            letra = "enter";
+    texto = texto.toLowerCase();
+    for (var key in encryptDict) {
+        if (texto.includes(key)) {
+            texto = texto.replaceAll(key, encryptDict[key]);
         }
-        else if (letra == "i") {
-            letra = "imes";
-        }
-        else if (letra == "a") {
-            letra = "ai";
-        }
-        else if (letra == "o") {
-            letra = "ober";
-        }
-        else if (letra == "ufat") {
-            letra = "c";
-        }
-        textoEncriptado += letra;
     }
-    return textoEncriptado;
+    return texto;
 }
 
 function desencriptador(texto) {
-    var textoDesencriptado = "";
-    var letra;
-    for (var i = 0; i < texto.length; i++) {
-        letra = texto[i]
-        if (letra == "enter") {
-            letra = "e";
+    texto = texto.toLowerCase();
+    let encryptDictInv = Object.entries(encryptDict).map(([key, value]) => [value, key]);
+    encryptDictInv = Object.fromEntries(encryptDictInv);
+
+    for (var key in encryptDictInv) {
+        if (texto.includes(key)) {
+            texto = texto.replaceAll(key, encryptDictInv[key]);
         }
-        else if (letra == "imes") {
-            letra = "i";
-        }
-        else if (letra == "ai") {
-            letra = "a";
-        }
-        else if (letra == "ober") {
-            letra = "o";
-        }
-        else if (letra == "c") {
-            letra = "ufat";
-        }
-        textoDesencriptado += letra;
     }
-    return textoDesencriptado;
+    return texto;
 }
 
-console.log(encriptador("hola"));
+
+
+document.querySelector("[data-button='encrypt']").addEventListener("click", function () {
+    var texto = document.querySelector("[data-text='input']").value;
+    texto_encriptado = encriptador(texto);
+
+    document.querySelector('.not-message').classList.replace('flex', 'hidden');
+    document.querySelector('.encriptado').classList.remove('hidden');
+    document.querySelector("[data-text='output']").innerHTML = texto_encriptado;
+
+;
+});
+
+document.querySelector("[data-button='decrypt']").addEventListener("click", function () {
+    var texto = document.querySelector("[data-text='input']").value;
+    texto_desencriptado = desencriptador(texto);
+
+    document.querySelector('.not-message').classList.replace('flex', 'hidden');
+    document.querySelector('.encriptado').classList.remove('hidden');
+    document.querySelector("[data-text='output']").innerHTML = texto_desencriptado;
+});
+
+document.querySelector("[data-button='copy']").addEventListener("click", function () {
+    var copyText = document.querySelector("[data-text='output']").innerHTML;
+    navigator.clipboard.writeText(copyText);
+} );
